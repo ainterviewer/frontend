10826f8dce98766f8f8872fc627907b39bebf789
gaardhus <tobias_gaardhus@hotmail.com>
2025-12-26 21:18:08 +0100

    change login image to png\

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -7,9 +7,9 @@
 	<!-- Left Side - Image -->
 	<div class="relative hidden overflow-hidden bg-secondary/20 lg:block lg:w-1/2">
 		<img
-			src="/images/login.png"
+			src="/images/login.webp"
 			alt="Login visual"
-			class="absolute inset-0 h-full w-full object-cover transition-opacity duration-500 {imageLoaded
+			class="absolute inset-0 h-full w-full object-cover transition-opacity duration-300 {imageLoaded
 				? 'opacity-100'
 				: 'opacity-0'}"
 			onload={() => (imageLoaded = true)}
