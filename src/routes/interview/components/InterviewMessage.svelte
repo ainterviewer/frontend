898b85e3c8c1b35127e637e56005088889c1a11e
gaardhus <tobias_gaardhus@hotmail.com>
2026-01-06 16:43:50 +0100

    add readonly property to InterviewMessage for i.e. feedback

--- src/routes/interview/components/InterviewMessage.svelte
+++ src/routes/interview/components/InterviewMessage.svelte
@@ -3,11 +3,21 @@
 	import FeedbackToggle from './FeedbackToggle.svelte';
 	import SkipButton from './SkipButton.svelte';
 
-	let { message, lang = 'en', isLast = false, onFeedback, onSkip, onSurveyAnswer } = $props();
+	let {
+		message,
+		lang = 'en',
+		isLast = false,
+		readonly = false,
+		onFeedback,
+		onSkip,
+		onSurveyAnswer
+	} = $props();
 
 	let isReceived = $derived(message.type === 'received');
 	// Only show controls for received messages that are not the intro (id > 1)
 	let showControls = $derived(isReceived && message.message_id && Number(message.message_id) > 1);
+
+	let showFeedbackIndicator = $derived(readonly && message.feedback);
 </script>
 
 <div
@@ -42,14 +52,36 @@
 	</div>
 
 	<!-- Controls (only for received) -->
-	{#if showControls}
-		<div class="ml-2 hidden items-center gap-2 self-center group-hover:flex">
-			<FeedbackToggle
-				feedback={message.feedback}
-				onFeedback={(f: any) => onFeedback(f, message.message_id)}
-			/>
-			{#if isLast}
-				<SkipButton {onSkip} />
+	{#if (showControls && !readonly) || showFeedbackIndicator}
+		<div
+			class="ml-2 items-center gap-2 self-center {showFeedbackIndicator
+				? 'flex'
+				: 'hidden group-hover:flex'}"
+		>
+			{#if readonly}
+				{#if message.feedback === 'positive'}
+					<div
+						class="flex h-8 w-8 items-center justify-center rounded-lg bg-green-50 text-green-600"
+						title="Positive feedback"
+					>
+						<i class="fa-solid fa-thumbs-up"></i>
+					</div>
+				{:else if message.feedback === 'negative'}
+					<div
+						class="flex h-8 w-8 items-center justify-center rounded-lg bg-red-50 text-red-600"
+						title="Negative feedback"
+					>
+						<i class="fa-solid fa-thumbs-down"></i>
+					</div>
+				{/if}
+			{:else}
+				<FeedbackToggle
+					feedback={message.feedback}
+					onFeedback={(f: any) => onFeedback(f, message.message_id)}
+				/>
+				{#if isLast}
+					<SkipButton {onSkip} />
+				{/if}
 			{/if}
 		</div>
 	{/if}
