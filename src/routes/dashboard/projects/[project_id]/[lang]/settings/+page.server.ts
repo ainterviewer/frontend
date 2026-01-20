ab0eb46b805c43f0e446c5574dbff4de98fdb68b
gaardhus <tobias_gaardhus@hotmail.com>
2026-01-20 19:59:19 +0100

    remove unused fetch from server call

--- src/routes/dashboard/projects/[project_id]/[lang]/settings/+page.server.ts
+++ src/routes/dashboard/projects/[project_id]/[lang]/settings/+page.server.ts
@@ -3,15 +3,14 @@ import type { ProjectPublic } from '$lib/api/types.gen';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ cookies, params, fetch }) => {
+export const load: PageServerLoad = async ({ cookies, params }) => {
 	const token = cookies.get('token') || '';
 	const { project_id } = params;
 
 	const [projectRes] = await Promise.all([
 		Projects.getProject({
 			auth: token,
-			path: { project_id: project_id },
-			fetch
+			path: { project_id: project_id }
 		})
 	]);
 
