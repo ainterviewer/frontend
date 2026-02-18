2b13b87c86da82bf289b331406c4756fa359fb04
gaardhus <tobias_gaardhus@hotmail.com>
2026-02-18 21:37:17 +0100

    fix tailwind size for AssistanceChat

--- src/lib/components/AssistanceChat.svelte
+++ src/lib/components/AssistanceChat.svelte
@@ -236,8 +236,8 @@
 {#if isOpen}
 	<div
 		class="fixed z-50 flex flex-col overflow-hidden rounded-2xl bg-white shadow-2xl ring-1 ring-black/10 transition-all duration-300 ease-in-out {isMaximized
-			? 'right-4 bottom-4 h-[800px] w-[1200px]'
-			: 'right-6 bottom-24 h-[500px] w-96'}"
+			? 'right-4 bottom-4 h-200 w-300'
+			: 'right-6 bottom-24 h-125 w-96'}"
 		transition:fly={{ y: 16, duration: 200 }}
 	>
 		<!-- Header -->
