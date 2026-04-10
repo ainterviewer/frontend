fcbd87c4ac63d4e985d56bfcc5aa6190f5362295
gaardhus <tobias_gaardhus@hotmail.com>
2026-04-10 12:13:07 +0200

    update dashboard layout with added padding

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import { auth } from '$lib/auth.svelte';
-	import { sidebar } from '$lib/sidebar.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import { sidebar } from '$lib/sidebar.svelte';
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
 
 	let { children, data }: { children: Snippet; data: LayoutData } = $props();
 
 	$effect(() => {
-		console.log(data.user);
 		auth.setUser(data.user);
 	});
 </script>
@@ -18,7 +17,7 @@
 <div class="bg-dark">
 	<main
 		class={[
-			'mt-11 flex h-[calc(100vh-2.75rem)] flex-col items-center overflow-scroll rounded-b-[10px] border-r-6 border-b-6 border-r-dark border-b-dark bg-light py-10 transition-all duration-500',
+			'mt-11 flex h-[calc(100vh-2.75rem)] flex-col items-center overflow-y-scroll rounded-b-[10px] border-r-6 border-b-6 border-r-dark border-b-dark bg-light py-10 transition-all duration-500',
 			sidebar.collapsed ? 'ml-[70px]' : 'ml-[250px]'
 		].join(' ')}
 	>
