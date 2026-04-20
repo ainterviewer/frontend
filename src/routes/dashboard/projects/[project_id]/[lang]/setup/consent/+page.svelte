c55f0ea8e039641572925cb1ac5f428fd29916d6
gaardhus <tobias_gaardhus@hotmail.com>
2026-04-20 13:26:40 +0200

    add language keys to consent and welcome page to trigger new data load

--- src/routes/dashboard/projects/[project_id]/[lang]/setup/consent/+page.svelte
+++ src/routes/dashboard/projects/[project_id]/[lang]/setup/consent/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import type { PageData } from './$types';
 	import Consent from './Consent.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-<Consent
-	initialData={data.consent}
-	availableLanguages={data.available_languages}
-	projectName={data.project_name}
-/>
+{#key page.params.lang}
+	<Consent
+		initialData={data.consent}
+		availableLanguages={data.available_languages}
+		projectName={data.project_name}
+	/>
+{/key}
--- src/routes/dashboard/projects/[project_id]/[lang]/setup/welcome/+page.svelte
+++ src/routes/dashboard/projects/[project_id]/[lang]/setup/welcome/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import type { PageData } from './$types';
 	import Welcome from './Welcome.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-<Welcome
-	initialData={data.welcome}
-	availableLanguages={data.available_languages}
-	projectName={data.project_name}
-/>
+{#key page.params.lang}
+	<Welcome
+		initialData={data.welcome}
+		availableLanguages={data.available_languages}
+		projectName={data.project_name}
+	/>
+{/key}
