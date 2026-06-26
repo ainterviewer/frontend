6ea52446224550fe1ba6c1f435c4be5307e1888b
gaardhus <tobias_gaardhus@hotmail.com>
2026-06-26 11:39:52 +0200

    fix typo

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -69,7 +69,7 @@
 					popover: {
 						title: 'Welcome to the AInterviewer platform 👋',
 						description:
-							'This interactive tour will guide you through the platforms features, and help you get started. Navigate the tour with the buttons below or with the arrow keys on your keyboard.'
+							"This interactive tour will guide you through the platform's features, and help you get started. Navigate the tour with the buttons below or with the arrow keys on your keyboard."
 					}
 				},
 				{
